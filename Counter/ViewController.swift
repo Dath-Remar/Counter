7f4fc54acc479c01e18457e144b23ab4c1a253f3
Sergei Balakirev <sergei-balak@yandex.ru>
2024-02-26 14:17:35 +0300

    попытка 2

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -8,12 +8,83 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var displayCounter: UILabel!
+    @IBOutlet weak var incrementButton: UIButton!
+    
+    @IBOutlet weak var resetButton: UIButton!
+    @IBOutlet weak var decrementButton: UIButton!
+    
+    @IBOutlet weak var changesHistoryTextView: UITextView!
+    
+    
+    var counter: Int = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setupInitialHistoryText()
+        changesHistoryTextView.isScrollEnabled = true
+        changesHistoryTextView.isEditable = false       
+        updateCounterLabel()
+    }
+    
+    
+    @IBAction func tapIncrementButton(_ sender: Any) {
+        counter += 1
+        updateCounterLabel()
+        appendToHistory(change: 1)
     }
 
+    @IBAction func tapDecrementButton(_ sender: Any) {
+        if counter > 0 {
+                    counter -= 1
+                    updateCounterLabel()
+                    appendToHistory(change: -1)
+                } else {
+                    appendToHistoryAttemptBelowZero()
+                }
+    }
+    
+    @IBAction func tapResetButton(_ sender: Any) {
+        counter = 0
+                updateCounterLabel()
+                appendToHistoryReset()
+            }
+    
+    
 
+    func updateCounterLabel() {
+        displayCounter.text = "Значение счётчика: \(counter)"
+    }
+    func setupInitialHistoryText() {
+            changesHistoryTextView.text = "История изменений:"
+        }
+    func appendToHistory(change: Int) {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "dd.MM.yyyy HH:mm:ss"
+        let dateString = dateFormatter.string(from: Date())
+        changesHistoryTextView.text.append("\n\(dateString): значение изменено на \(change)")
+        scrollToBottom()
+    }
+    func appendToHistoryAttemptBelowZero() {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "dd.MM.yyyy HH:mm:ss"
+        let dateString = dateFormatter.string(from: Date())
+        changesHistoryTextView.text.append("\n\(dateString): попытка уменьшить значение счётчика ниже 0")
+        scrollToBottom()
+    }
+    func appendToHistoryReset() {
+            let dateFormatter = DateFormatter()
+            dateFormatter.dateFormat = "dd.MM.yyyy HH:mm:ss"
+            let dateString = dateFormatter.string(from: Date())
+            changesHistoryTextView.text.append("\n\(dateString): значение сброшено")
+            scrollToBottom()
+        }
+    
+    func scrollToBottom() {
+            let bottom = NSMakeRange(changesHistoryTextView.text.count - 1, 1)
+            changesHistoryTextView.scrollRangeToVisible(bottom)
+        }
+    
 }
 
